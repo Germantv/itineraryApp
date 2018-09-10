2c2a6c2fde0758b02f496aa2ae22816e857d8ac0
German Flores <gfloresa@ucsd.edu>
2018-09-10 12:38:35 -0700

    add swipe feature to delete trip

--- Itinerary/Views/TripsViewController/TripsViewController.swift
+++ Itinerary/Views/TripsViewController/TripsViewController.swift
@@ -63,4 +63,31 @@ extension TripsViewController: UITableViewDataSource, UITableViewDelegate {
         return 160
     }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let trip = Data.tripModels[indexPath.row]
+        
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { (contextualAction, view, actionPerformed: @escaping (Bool) -> ()) in
+            
+            let alert = UIAlertController(title: "Delete Trip", message: "Are you sure you want to delete this trip: \(trip.title!) ?", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (alertAction) in
+                actionPerformed(false)
+            }))
+            
+            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (alertAction) in
+                // Perform delete
+                TripFunctions.deleteTrip(index: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+                actionPerformed(true)
+            }))
+            
+            self.present(alert, animated: true)
+        }
+        delete.image = UIImage(cgImage: #imageLiteral(resourceName: "delete").cgImage!, scale: 2.5, orientation: .up)
+        delete.backgroundColor = #colorLiteral(red: 0.5916612413, green: 0.4012586806, blue: 0.4087456597, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [delete])
+    }
+    
 }
