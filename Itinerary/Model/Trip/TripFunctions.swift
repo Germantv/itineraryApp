e40c4bc0650cc49251e642cdb7b1fe60136a8ce3
German Flores <gfloresa@ucsd.edu>
2018-09-09 17:08:10 -0700

    added model and functions for Trip

---
+++ Itinerary/Model/Trip/TripFunctions.swift
@@ -0,0 +1,27 @@
+//
+//  TripFunctions.swift
+//  Itinerary
+//
+//  Created by German Flores on 9/9/18.
+//  Copyright © 2018 German Flores. All rights reserved.
+//
+
+import Foundation
+
+class TripFunctions {
+    static func createTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func readTrips() {
+        
+    }
+    
+    static func updateTripModel(tripModel: TripModel) {
+        
+    }
+    
+    static func deleteTrip(tripModel: TripModel) {
+        
+    }
+}
